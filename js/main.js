e6d23f2d147725781c891ac0b7620230c9fbf71d
Aruljk <aruljk007@gmail.com>
2022-08-30 13:56:10 +0530

    project

---
+++ js/main.js
@@ -0,0 +1,200 @@
+//1//
+var bike={
+  name:'re',
+  model:'classic',
+  enginecapacity:350,
+  weight:180,
+  isalive:true,
+  color:'black',
+}
+ 
+
+  console.log(bike.name);
+
+//2//
+  var car={
+    model:"wolkswagan",
+    model:"x6",
+    weight:"1000kg",
+    color:"white",
+  }
+  console.log(car);
+
+//3//
+  var building={
+    model:"paintshop",
+    size:"10*10",
+    material:"paints",
+    color:"multi"
+    
+  }
+  console.log(building);
+//4//
+  var laptop={
+    brand:"Sony",
+    model:"ryzen" ,
+    color:"Blue",
+    emioption:"yes available",
+    price:58000,
+  }
+  console.log(laptop);
+//5//
+
+var mobile={
+  brand:"vivo",
+  model:"y51",
+  color:"blue",
+  quality:"1st",
+  price:18000,
+
+}
+console.log(mobile);
+//6//
+var actor={
+  name:"vijay",
+  language:"tamil",
+  age:48,
+  recentmovie:"beast",
+  bestmovie:"thupakki",
+
+}
+console.log(actor);
+//7//
+var native={
+  state:"tamilnadu",
+  district:"tenkasi",
+  favoriteplace:"courtalam",
+  favoritefood:"parotta",
+
+}
+console.log(native);
+//8//
+var movie={
+  name:'mersal',
+  hero:'vijay',
+  heroine:'samantha',
+  director:'Atlee' ,
+  musicdirector:'Arr' ,
+}
+console.log(movie);
+//9//
+var sports={
+  name:"cricket",
+  types:"20,50,test",
+  players:11,
+  umpires:3,
+  ball:"6perover"
+
+}
+console.log(sports);
+//10//
+var director={
+  name:"atlee",
+  age:35,
+  language:"tamil",
+  best:"mersel",
+  collection:"250cr"
+
+}
+console.log(director);
+
+//11//
+var tamilnadu={
+  language:"tamil",
+  governer:"R N RAVI",
+  Chiefminister:"MKStalin",
+  population:"6.79 crores",
+  capital:"chennai"
+}
+console.log(tamilnadu);
+//12//
+var chennai={
+  country:"india",
+  state:"tamilnadu",
+  district:"chennai",
+  formername:"madras" ,
+  favoriteplace:"marina"
+}
+console.log(chennai);
+//13//
+var mahindra={
+  brand:"mahindra-car",
+  model:"xuv700",
+  color:"Blue",
+  seatingcapacity:"5seater",
+  transmission:"manual"
+
+}
+console.log(mahindra);
+//14//
+var infosys={
+  ceo:"salilparekh",
+  headquarters:"bengaluru",
+  numberofemployee:"2,76,319",
+  competitors:"wipro",
+  
+}
+console.log(infosys);
+//15//
+var html={
+  fullform:"hypertextmarkuplanguage",
+  containedby:"webbrowser",
+  initialrelease:"29years ago",
+  typecode:"text",
+  containerfor:"html elements"
+}
+console.log(html);
+//16//
+var facebook={
+  ceo:"markzuckerberg",
+  headquarters:"california",
+  founded:"february",
+  industry:"socialmedia",
+  numberofemployee:"83,533",
+  
+}
+console.log(facebook);
+//17//
+var css={
+  fullform:"cascading style sheets",
+  developedby:"world wide web",
+  initialrelease:"25years ago",
+  typeofformat:"style sheet language",
+  containedfor:"html elements"
+}
+console.log(css);
+//18//
+
+var goa={
+  country:"india",
+  largestcity:"vascodama" ,
+  capital:"panaji",
+  population:"18.2lakhs",
+  founded:"30may1987",
+}
+console.log(goa);
+//19//
+var aircraft={
+  dassaultrafale:"france",
+  HALTejas:"india",
+  MIG:"russia",
+  jaguar:"uk",
+  HALRudra:"india",
+
+}
+console.log(aircraft);
+//20//
+var instagram={
+  ceo:"adammosseri",
+  headquarters:"california",
+  founded:"2010",
+  industry:"socialmedia",
+  numberofemployee:"83,533",
+  
+}
+console.log(instagram);
+
+
+
+
+
